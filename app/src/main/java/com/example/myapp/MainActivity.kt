cbd3bdc00ee58e850eba59014005051ac7c31897
aldanziikri <aldanzikri23398@gmail.com>
2025-05-23 21:14:55 +0700

    initial commit + coba buat interface hitung

---
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.myapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    var number: Long = 0
+
+    fun hitung(){
+        number++
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button: Button = findViewById(R.id.button_view)
+        val loadingTxt: TextView = findViewById(R.id.loading_text)
+
+        button.setOnClickListener({
+            hitung()
+            loadingTxt.text = this.number.toString()
+        })
+
+    }
+}
